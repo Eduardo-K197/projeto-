aa3c58405020516a78c4e220ab38fe2e2b7847db
Eduardo.K197 <44842173+Eduardo-K197@users.noreply.github.com>
2022-03-19 02:13:43 -0300

    inserindo banco 3.4

--- conexao2.php
+++ conexao2.php
@@ -1,11 +1,12 @@
 <?php
+
 $servidor="52.21.128.106";
 $usuario="Eduardo";
 $senha="353331499Eduardo";
 $banco="cadastro";
 
 $conexao=mysqli_connect($servidor, $usuario, $senha, $banco);
-if(!$conexao){
-    die("Houve um erro: ".mysqli_connect_error());
-}
+    if(!$conexao){
+        die( "Não pode conectar: ".mysqli_connect_error());
+    }
 ?>
